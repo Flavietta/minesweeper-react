f4a4a9453eed076c4ddb9bd91e0e316b4538d671
flavuoresce <saidi3mahdi@gmail.com>
2019-03-12 11:28:05 -0400

    Empty Box Handler
    This changes handle the click on an empty box that has no adjacent mine

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import "./main.css"
 import { Grid } from './grid.js'
-import { notDeepEqual } from 'assert';
 
 export class Main extends Component {
 
@@ -13,18 +12,22 @@ export class Main extends Component {
         }
     }
 
+
+
     checkMine = (i, j) => {
 
         var newGrid = JSON.parse(JSON.stringify(this.state.grid));
-        console.log(newGrid);
-        console.log(i + "  " + j);
+
         newGrid[i][j] = {
             visible: true,
             content: this.state.grid[i][j].content
         };
+        if (newGrid[i][j].content === 0) {
+            var grid = showAdjascent(newGrid, i, j);
+        }
         console.log(newGrid);
         this.setState({
-            grid: newGrid
+            grid: grid
         })
     }
 
@@ -37,23 +40,29 @@ export class Main extends Component {
 
         for (let index = 0; index < 5; index++) {
             var i = Math.round(Math.random() * 10) - 1;
-            var j = Math.round(Math.random() * 5) - 1;
+            var j = Math.round(Math.random() * 9) - 1;
             while (grid[i][j].content === "mine") {
                 i = Math.round(Math.random() * 10) - 1;
-                j = Math.round(Math.random() * 5) - 1;
+                j = Math.round(Math.random() * 9) - 1;
             }
+            console.log("random i " + i);
+
+            console.log("random j " + j);
             newGrid[i][j] = {
                 visible: false,
                 content: "mine",
             };
             for (let ix = -1; ix < 2; ix++) {
                 for (let jx = -1; jx < 2; jx++) {
-                    if (i + ix >= 0 && j + jx >= 0 && i + ix <= newGrid.length && j + jx <= newGrid.length && newGrid[i + ix][j + jx] != "mine") {
-                        console.log(i + ix + "  xxx   " + j + jx);
-                        newGrid[i + ix][j + jx] = {
-                            visible: false,
-                            content: newGrid[i + ix][j + jx].content + 1,
-                        };
+                    if (i + ix >= 0 && j + jx >= 0 && i + ix <= 10 && j + jx <= 9) {
+                        if (newGrid[i + ix][j + jx] != "mine") {
+
+                            console.log(i + ix + "  xxx   " + j + jx);
+                            newGrid[i + ix][j + jx] = {
+                                visible: false,
+                                content: newGrid[i + ix][j + jx].content + 1,
+                            };
+                        }
                     }
 
                 }
@@ -80,4 +89,22 @@ export class Main extends Component {
             </div>
         )
     }
+}
+
+function showAdjascent(grid, i, j) {
+    for (let ix = -1; ix < 2; ix++) {
+        for (let jx = -1; jx < 2; jx++) {
+            if (i + ix >= 0 && j + jx >= 0 && i + ix <= 10 && j + jx <= 9) {
+
+                grid[i][j] = {
+                    visible: true,
+                    content: grid[i][j].content
+                };
+                if (grid[i + ix][j + jx].content === 0) {
+                    showAdjascent(grid, ix, jx);
+                }
+            }
+        }
+    }
+    return grid;
 }
